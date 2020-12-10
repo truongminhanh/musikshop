12702a2da63d32c5f4e45a195c755452fb7ebca3
minh <truongminhanh@web.de>
2020-12-10 20:04:27 +0100

    login gebaut

--- src/main/java/com/example/musikshop/config/ThymeleafConfiguration.java
+++ src/main/java/com/example/musikshop/config/ThymeleafConfiguration.java
@@ -1,14 +1,12 @@
 package com.example.musikshop.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
-@Configuration
 public class ThymeleafConfiguration {
 
     @Bean
-    public SpringSecurityDialect springSecurityDialect (){
+    public SpringSecurityDialect springSecurityDialect(){
         return new SpringSecurityDialect();
     }
 }
